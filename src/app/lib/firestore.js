e1725c764096193ee738d1bc54c4d17185bb5565
afixv <afixv00@gmail.com>
2024-10-21 13:53:40 +0700

    feat: add updateStatus function in firestore.js

--- src/app/lib/firestore.js
+++ src/app/lib/firestore.js
@@ -1,12 +1,35 @@
-import { collection, getDocs, doc, deleteDoc } from 'firebase/firestore';
-import { db } from './firebase';
+import {
+  collection,
+  getDocs,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "./firebase";
 
 export const getAllDocuments = async (collectionName) => {
   const snapshot = await getDocs(collection(db, collectionName));
-  const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+  const data = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   return data;
 };
 
 export const deleteDocument = async (collectionName, id) => {
   await deleteDoc(doc(db, collectionName, id));
+};
+
+export const updateStatusDocument = async (
+  collectionName,
+  id,
+  status,
+  notes = ''
+) => {
+  const updates = { status };
+
+  if (status === "Ditanggapi" && notes) {
+    updates.notes = notes;
+  }
+
+  console.log(updates);
+
+  await updateDoc(doc(db, collectionName, id), updates);
 };
